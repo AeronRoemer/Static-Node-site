ba985e738cac949fb652711e746e781a72b53930
Aeron Roemer <57688724+AeronRoemer@users.noreply.github.com>
2020-03-20 18:05:17 -0600

    Error Page

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express');
-const {data} = require('data.json');
+const data = require('./data.json');
 const app = express();
 
 //sets the view engine as pug for render requests
+//looks for pug templates in default views file
 app.set('view engine', 'pug');
 
 //serve static files
 app.use('/static', express.static('public'));
 
 //serves routes  -----  -- - mus create /routes - - - - - - - -
-const mainRoutes = require('./routes');
+const routes = require('./routes');
+//uses the routes imported from the routes file & index.js contained within
+app.use(routes);
 
 //404 and other error handler 
 //creates error
@@ -27,10 +30,10 @@ app.use((req, res, next)=>{
     else
       res.status(500);
 //needs error page created - -  - - - - --  -- - - - - - -- - -
-    res.render('error');
+    res.render('error', {err});
   });
-  
+
   //starts listening on specfied port(localhost:3000). Server must be started with node app.js in terminal
 app.listen(3000, () =>{
     console.log('The server is up and running')
-});
+}); 
